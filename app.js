5c907461ab77873d6dd6e9fc4a7059024f695759
João Juanutti <joaojuanutti33@gmail.com>
2023-10-30 15:11:57 -0300

    voltando atrás

--- app.js
+++ app.js
@@ -1,4 +1,2 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("aplicação está se conectado ao banco de dados")
-// mais um console log de teste:
-console.log("Enviando para o GitHub")
